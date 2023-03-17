381ac0462e1ab607a10cbccb12f719013bd61bd6
RoySimon <rxts11562486@163.com>
2023-03-17 15:34:14 +0800

    update Alter_Auth.py

--- find_regInfo/Alter_Auth.py
+++ find_regInfo/Alter_Auth.py
@@ -2,6 +2,17 @@ import win32api
 import win32con
 
 
-key = win32api.RegOpenKeyEx(win32con.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
-                            0, win32con.KEY_SET_VALUE)
-print(key)
+# 获取注册表值 不存在返回False
+def getRegValue(handle, key):
+    try:
+        return win32api.RegQueryValueEx(handle, key)[0]
+    except Exception as e:
+        return False
+
+
+key = 'Nextcloud'
+handle = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
+                               0, win32con.KEY_ALL_ACCESS)
+# 获取注册表的值
+values = getRegValue(handle, key)
+print(values)
