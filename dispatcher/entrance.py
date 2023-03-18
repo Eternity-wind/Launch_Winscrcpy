9a547a518e6547d415e702deb19983c5eca82ad7
Eternity-wind <rxts11562486@163.com>
2023-03-18 17:52:41 +0800

    complete the pyinstaller package

--- dispatcher/entrance.py
+++ dispatcher/entrance.py
@@ -9,7 +9,7 @@ import win32con
 
 # 执行scrcpy.exe
 def exe_scrcpy():
-    adb_path = 'C:/Users/11561/Desktop/scrcpy-win64-v2.0'
+    adb_path = './scrcpy-win64-v2.0'
     os.chdir(adb_path)
     # print(os.getcwd())
     cmd_check_devices = 'adb.exe devices'
